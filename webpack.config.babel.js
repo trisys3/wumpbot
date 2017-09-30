1d79e579b37be8cb095ef5efa9cb31777f38a2f8
Sean Moran <sean_r_moran@msn.com>
2017-09-30 16:43:57 -0400

    Added webpack configuration

---
+++ webpack.config.babel.js
@@ -0,0 +1,82 @@
+import {HotModuleReplacementPlugin as HMR} from 'webpack';
+import IndexHtml from 'html-webpack-plugin';
+
+const hmr = new HMR();
+const indexHtml = new IndexHtml();
+
+const plugins = [hmr, indexHtml];
+
+const jsLoader = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  options: {cacheDirectory: true},
+};
+
+const cssLoader = {
+  test: /\.css$/,
+  use: [
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true,
+        minimize: false,
+        import: false,
+        modules: true,
+        camelCase: 'only',
+        importLoaders: 1,
+      },
+    },
+    'postcss-loader',
+  ],
+};
+
+if(process.env.nodeEnv === 'production') {
+  cssLoader.use.unshift(
+    {
+      loader: 'file-loader',
+      options: {name: '[name].css'},
+    },
+    'extract-loader',
+  );
+}
+
+const imgLoader = {
+  test: /\.(png|jpe?g|gif|svg)$/i,
+  use: [{
+    loader: 'img-loader',
+    options: {minimize: true},
+  },
+  {
+    loader: 'url-loader',
+    options: {name: '[sha512:hash].[ext]'},
+  }],
+};
+
+const htmlLoader = {
+  test: /\.html$/,
+  loader: 'html-loader',
+};
+
+const loaders = [
+  jsLoader,
+  cssLoader,
+  imgLoader,
+  htmlLoader,
+];
+
+export default {
+  entry: {app: './app.js'},
+  output: {
+    filename: '[name]/[hash].bundle.js',
+    chunkFilename: '[name]/[hash]/chunks/[chunkhash].chunk.js',
+    path: `${process.cwd()}/dist/`,
+    hotUpdateMainFilename: 'hot/[hash].update.json',
+    hotUpdateChunkFilename: 'hot/[id].[hash].update.js',
+    pathinfo: process.NODE_ENV === 'development',
+  },
+  plugins,
+  module: {rules: loaders},
+  devtool: 'source-map',
+  recordsPath: `${__dirname}/records.json`,
+};
