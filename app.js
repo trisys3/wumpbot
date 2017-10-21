225e071ae5a7f54089e77206da8f929722031a99
Sean Moran <sean_r_moran@msn.com>
2017-10-20 20:46:15 -0400

    Added hot module replacement; added some dependencies to package.json that I forgot to add before

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 
 import './app.css';
 import WbMap from './map';
 
+const socket = io();
+
 const mapElem = global.document.querySelector('map');
 
 ReactDOM.render(<WbMap />, mapElem);
+
+if(module.hot) {
+  module.hot.accept();
+  socket.once('hot-update', checkHot);
+}
+
+async function checkHot() {
+  const modules = await module.hot.check(true);
+  if(!Array.isArray(modules) || !modules.length) {
+    socket.once('hot-update', checkHot);
+  }
+}
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const env = process.env.NODE_ENV || 'development';
 
-import yargs from 'yargs';
+import {options as cliArgs} from 'yargs';
 import {createServer} from 'http';
 import Koa from 'koa';
 import compress from 'koa-compress';
 import serveStatic from 'koa-static';
-import {green, gray, magenta, cyan, red} from 'chalk';
+import {green, gray, magenta, cyan, red, blue} from 'chalk';
+import webpack from 'webpack';
+import Sockets from 'socket.io';
 
-const {argv} = yargs.options({
+import bundler from './webpack.config.babel';
+
+const socket = new Sockets();
+
+const {argv} = cliArgs({
   port: {
     alias: 'r',
     type: 'number',
@@ -17,13 +23,54 @@ const {argv} = yargs.options({
 });
 
 const server = new Koa();
+const bundle = webpack(bundler);
 
 server.use(compress())
   .use(serveStatic('dist/'));
 
 const app = createServer(server.callback());
+socket.listen(app);
 app.listen(argv.port);
 console.log(green('Application'), cyan('Wumpbot'));
 console.log(green('Started at'), red(new Date()));
 console.log(green('Port'), gray(argv.port));
 console.log(green('Environment:'), magenta(env));
+
+bundle.watch({}, (err, stats) => {
+  if(err) {
+    console.log(err.stack || err);
+    if(err.details) {
+      console.error(err.details);
+    }
+    return;
+  }
+
+  const {hash, time, warnings, errors} = stats.toJson({
+    assets: false,
+    cached: false,
+    cachedAssets: false,
+    children: false,
+    chunks: false,
+    chunkModules: false,
+    chunkOrigins: false,
+    modules: false,
+    publicPath: false,
+    reasons: false,
+    source: false,
+    version: false,
+  });
+
+  console.log(green('Bundle'), blue(hash), green('finished in'),
+    gray(`${time} ms`));
+
+  if(stats.hasErrors()) {
+    console.error(errors);
+    return;
+  }
+
+  if(stats.hasWarnings()) {
+    console.warn(warnings);
+  }
+
+  socket.emit('hot-update');
+});
