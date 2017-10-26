935b4185bf09efeb58038d237eb56b81bce532f8
Sean Moran <sean_r_moran@msn.com>
2017-10-26 07:18:56 -0400

    Removed the map image; preferred SVG when drawing on the map for better external style-ability & easier debugging; added map maximum bounds & initial center & zoom level, which will most likely need to be tweaked later but which provide a starting point

--- map/index.js
+++ map/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Map as ReactMap, ImageOverlay} from 'react-leaflet';
+import {Map as ReactMap} from 'react-leaflet';
 import L from 'leaflet';
 
 import './index.css';
 
+const maxBounds = [[0, 0], [10000, 10000]];
+const center = [5000, 5000];
+
 export default function map() {
-  return <ReactMap preferCanvas={true} zoomControl={false} crs={L.CRS.Simple} attributionControl={false}>
-    <ImageOverlay url='map-bg.png' />
-  </ReactMap>;
+  return <ReactMap zoomControl={false} crs={L.CRS.Simple} attributionControl={false} maxBounds={maxBounds} center={center} zoom={13} />;
 }
