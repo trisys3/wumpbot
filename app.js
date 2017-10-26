5e65af3ae4ed8b32212b55bd7b21642f457746dc
Sean Moran <sean_r_moran@msn.com>
2017-10-26 07:09:31 -0400

    Added react-hot-loader to provide better hot module replacement in react

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import 'react-hot-loader/patch';
+import {AppContainer} from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import io from 'socket.io-client';
@@ -9,16 +11,26 @@ const socket = io();
 
 const mapElem = global.document.querySelector('map');
 
-ReactDOM.render(<WbMap />, mapElem);
+render();
 
 if(module.hot) {
   module.hot.accept();
+  module.hot.accept('./map', () => render());
   socket.once('hot-update', checkHot);
 }
 
 async function checkHot() {
   const modules = await module.hot.check(true);
   if(!Array.isArray(modules) || !modules.length) {
+    // most likely, the server was restarted, meaning we can't do a hot-update
+    // we should think about giving a message in the browser or just forcing a
+    // full reload
     socket.once('hot-update', checkHot);
   }
+}
+
+function render() {
+  return ReactDOM.render(<AppContainer>
+    <WbMap />
+  </AppContainer>, mapElem);
 }
